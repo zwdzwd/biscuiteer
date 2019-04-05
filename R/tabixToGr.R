0993293681c953fbca3e0cff55c57a0bde208244
Wanding Zhou <wanding.zhou@vai.org>
2019-04-05 13:18:28 -0400

    added tabix to matrix and tabix to table

--- R/tabixToGr.R
+++ R/tabixToGr.R
@@ -1,27 +1,21 @@
 
-#' Read from tabix-indexed bed file to GRanges or tibble
-#' The fourth column is DNA methylation level and the fifth is depth.
-#' This is also the default output of Biscuit BED.
+#' Read from tabix-indexed bed file to list objects
 #' 
 #' @param paths paths to the bed files
 #' @param chrm  chromosome name
 #' @param beg   start coordinate of CpG
 #' @param end   end coordinate of CpG 2^29 is the max taken
-#' @param as_gr whether to output GRanges
-#' @param min_depth mask all depth < min_depth as NA. This is useful as depths
-#' are not recorded in the output
 #' @param sample_names sample names, just use paths if not specified
 #' @param BPPARAM how to parallelize
-#' @return a tibble or GRanges object with DNA methylation level
+#' @return a list object with DNA methylation level and depth
 #'
-#' @import GenomicRanges
 #' @import BiocParallel
 #' @import dplyr
 #'
 #' @export
-tabixToGr <- function(
+tabixRetrieve <- function(
     paths, chrm, beg = 1, end = 2^28,
-    as_gr = FALSE, min_depth = 0, sample_names = NULL,
+    min_depth = 0, sample_names = NULL,
     BPPARAM = SerialParam()) {
 
     input_range <- GRanges(chrm, IRanges(beg, end))
@@ -33,7 +27,8 @@ tabixToGr <- function(
         df$beg <- as.numeric(df$beg)
         df$beg <- df$beg + 1
         df$end <- as.numeric(df$end)
-        df$beta[df$beta == '.'] <- NA
+        ## in case the tabix is mal-formed
+        df$beta[df$beta == '.' | df$beta == 'NA'] <- NA
         df$beta <- as.numeric(df$beta)
         df$depth <- as.integer(df$depth)
         df$depth[is.na(df$beta)] <- 0
@@ -45,26 +40,82 @@ tabixToGr <- function(
         function(i) identical(df_list[[i]][,1:3], df_list[[i+1]][,1:3]))
     stopifnot(all(same_coordinates))
 
-    ## collate data
-    data <- lapply(df_list, function(df) {
-        ifelse(df$depth >= min_depth, df$beta, NA)
-    })
-
     ## set sample names
     if (is.null(sample_names)) {
         sample_names <- paths
     }
     stopifnot(length(sample_names) == length(paths))
-    names(data) <- sample_names
+    names(df_list) <- sample_names
 
-    ## output either as GR or a tibble
-    if (as_gr) {
-        res <- GRanges(
-            seqnames = df_list[[1]]$chrm,
-            ranges = IRanges(df_list[[1]]$beg, df_list[[1]]$end))
-        mcols(res) <- as.data.frame(data)
-    } else {
-        res <- bind_cols(df_list[[1]][,1:3], as.tibble(data))
-    }
+    df_list
+}
+
+#' Read from tabix-indexed bed file into a tibble
+#'
+#' @param ... tabixRetrieve arguments
+#' @param min_depth minimum depth as depth is discarded in GRanges
+#' @return a tibble of DNA methylation level
+#' @import dplyr
+#' @seealso tabixRetrieve
+#'
+#' @export
+tabixToTable <- function(..., min_depth = 0) {
+    df_list <- tabixRetrieve(...)
+
+    ## collate data (methylation levels)
+    data <- lapply(df_list, function(df) {
+        ifelse(df$depth >= min_depth, df$beta, NA)
+    })
+    bind_cols(df_list[[1]][,1:3], as.tibble(data))
+}
+
+#' Read from tabix-indexed bed file into a matrix
+#' The row names are the CpG coordinates. The column names are
+#' sample names.
+#'
+#' @param ... tabixRetrieve and tabixToTable arguments
+#' @param min_depth minimum depth as depth is discarded in GRanges
+#' @return a matrix of DNA methylation level
+#' @seealso tabixRetrieve
+#' @seealso tabixToTable
+#'
+#' @export
+tabixToMatrix <- function(...) {
+
+    df_list <- tabixRetrieve(...)
+
+    ## collate data (methylation levels)
+    mat <- do.call(cbind, lapply(df_list, function(df) {
+        as.numeric(ifelse(df$depth >= min_depth, df$beta, NA))
+    }))
+    colnames(mat) <- names(df_list)
+    rownames(mat) <- paste0(
+        df_list[[1]]$chrm, ':',
+        df_list[[1]]$beg, '-',
+        df_list[[1]]$end)
+    mat
+}
+
+#' Read from tabix-indexed bed file into a GRanges
+#'
+#' @param ... tabixRetrieve arguments
+#' @param min_depth minimum depth as depth is discarded in GRanges
+#' @return a GenomicRanges of DNA methylation level
+#' @seealso tabixRetrieve
+#' @import GenomicRanges
+#'
+#' @export
+tabixToGr <- function(..., min_depth = 0) {
+    df_list <- tabixRetrieve(...)
+
+    ## collate data (methylation levels)
+    data <- lapply(df_list, function(df) {
+        ifelse(df$depth >= min_depth, df$beta, NA)
+    })
+    
+    res <- GRanges(
+        seqnames = df_list[[1]]$chrm,
+        ranges = IRanges(df_list[[1]]$beg, df_list[[1]]$end))
+    mcols(res) <- as.data.frame(data)
     res
 }
